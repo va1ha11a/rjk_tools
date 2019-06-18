fbc51735fac6729f17017fde98821fd7fc7c9e74
Robbie K <robbie@merge42.com>
2019-06-18 14:44:27 +1000

    Add some todos

--- decorators.py
+++ decorators.py
@@ -6,11 +6,15 @@ def dynamic_programming(func):
     Functions should be pure and not have external factors (such as external web calls)
     as this could give incorrect results.
     """
+    # TODO: Check safty accross many functions using it simutainously. I belive this should be ok
+    # as the closure containse the _store and I have checked a simple example.
+
+    # TODO: Add other backends such as flatfile or db as options
     _store = {}
 
     def inner(*args, **kwargs):
-        key = (args, tuple(sorted(kwargs)), tuple(
-            [kwargs[k] for k in sorted(kwargs)]))
+        key = (args, tuple(sorted(kwargs)),
+               tuple([kwargs[k] for k in sorted(kwargs)]))
         prev = _store.get(key)
         if prev != None:
             # 'returning from cache'
@@ -20,4 +24,5 @@ def dynamic_programming(func):
             ret = func(*args, **kwargs)
             _store[key] = ret
             return ret
+
     return inner
